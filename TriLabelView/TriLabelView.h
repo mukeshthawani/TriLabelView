6fe560609381501050127e08a28160ab572c0123
Mukesh Thawani <mukesh9039@gmail.com>
2016-07-10 13:30:25 +0530

    Initialized repository

---
+++ TriLabelView/TriLabelView.h
@@ -0,0 +1,19 @@
+//
+//  TriLabelView.h
+//  TriLabelView
+//
+//  Created by Mukesh Thawani on 10/07/16.
+//  Copyright © 2016 Mukesh Thawani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TriLabelView.
+FOUNDATION_EXPORT double TriLabelViewVersionNumber;
+
+//! Project version string for TriLabelView.
+FOUNDATION_EXPORT const unsigned char TriLabelViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TriLabelView/PublicHeader.h>
+
+
