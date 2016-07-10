452beb30af4ee3f56244c2441a6c01180f2600b4
Mukesh Thawani <mukesh9039@gmail.com>
2016-07-10 20:09:57 +0530

    New target demo

---
+++ Demo-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo-iOS
+//
+//  Created by Mukesh Thawani on 10/07/16.
+//  Copyright © 2016 Mukesh Thawani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
