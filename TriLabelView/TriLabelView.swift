cc3cfcbadb1c83bcfea7c89c8db4c705f945add9
Mukesh Thawani <mukesh9039@gmail.com>
2016-07-10 19:25:21 +0530

    Add second label

--- TriLabelView/TriLabelView.swift
+++ TriLabelView/TriLabelView.swift
@@ -10,6 +10,10 @@ import Foundation
 
 @IBDesignable class TriLabelView: UIView {
     
+    let secondLabel = UILabel()
+    var length = CGFloat()
+    var viewSize = CGFloat()
+    
     @IBInspectable var position: Position = .TopLeft {
         didSet {
             setNeedsDisplay()
@@ -22,15 +26,21 @@ import Foundation
         }
     }
     
+    @IBInspectable var firstLabelText:String = "Hi"
+    
     
     override func drawRect(rect: CGRect) {
         let trianglePath = UIBezierPath()
         var pointValues = [CGFloat]()
-        let length = (lengthPercentage/100)*min(bounds.width, bounds.height)
-        let viewSize = min(bounds.width, bounds.height)
+        length = (lengthPercentage/100)*min(bounds.width, bounds.height)
+        viewSize = min(bounds.width, bounds.height)
         switch position {
         case .TopRight:
             pointValues = [viewSize, 0, viewSize, length, viewSize - length, 0, viewSize, 0]
+            //        case .BottomLeft:
+            //
+            //        case .BottomRight:
+            
         default:
             // Default is TopLeft
             pointValues = [0, 0, 0, length, length, 0, 0, 0]
@@ -46,6 +56,43 @@ import Foundation
         trianglePath.lineWidth = 3
         trianglePath.fill()
         trianglePath.stroke()
+        addSecondLabelView()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSecondLabelView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        addSecondLabelView()
+    }
+    
+    
+    private func addSecondLabelView() {
+        var x = CGFloat()
+        var y = CGFloat()
+        length = (lengthPercentage/100)*min(bounds.width, bounds.height)
+        viewSize = min(bounds.width, bounds.height)
+        switch position {
+        case .TopRight:
+            x = viewSize
+            y = 0.1 * length
+            //        case .BottomRight:
+            //
+            //        case .BottomLeft:
+        //
+        default:
+            x = 0
+            y = 0.1 * length
+        }
+        let firstLabel = UILabel()
+        firstLabel.frame = CGRectMake(x, y, length, 20)
+        firstLabel.text = firstLabelText
+        firstLabel.transform = CGAffineTransformMakeRotation(3*(3.14/2) + (3.14/4))
+        firstLabel.backgroundColor = UIColor.brownColor()
+        self.addSubview(firstLabel)
     }
     
 }
