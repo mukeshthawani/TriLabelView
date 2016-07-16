a78b713b6c5151ea73ba8f6a601c9b9afdaf32da
Mukesh Thawani <mukesh9039@gmail.com>
2016-07-17 04:42:44 +0530

    update cell background and add labelview

--- Demo-iOS/ViewController.swift
+++ Demo-iOS/ViewController.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import TriLabelView
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    private var colorList:[UIColor] = [.cyanColor(),.magentaColor(),.purpleColor(),.grayColor(),.redColor(),.blueColor()]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -27,7 +29,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
                                sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         let viewWidth = view.frame.width
         let viewHeight = view.frame.height
-        let cellSize = CGSize(width: (viewWidth/2 - 1.0), height: viewHeight/3)
+        let cellSize = CGSize(width: viewWidth/2, height: viewHeight/3)
         return cellSize
     }
     
@@ -39,9 +41,10 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
-        cell.backgroundColor = UIColor.whiteColor()
+        let triLabel = TriLabelView(frame: cell.bounds)
+        cell.addSubview(triLabel)
+        cell.backgroundColor = colorList[indexPath.row]
         return cell
     }
-
 }
 
